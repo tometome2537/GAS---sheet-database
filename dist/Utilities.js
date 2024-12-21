29432db9ab6a5431e49930177e500f9f520a1b2f
とめとめ <146919125+tometome2537@users.noreply.github.com>
2024-12-22 05:34:29 +0900

    ファイル分割

---
+++ dist/Utilities.js
@@ -0,0 +1,8 @@
+function validate_(params) {
+  Object.keys(params).forEach(function(name) {
+    var value = params[name];
+    if (!value) {
+      throw new Error(name + ' is required.');
+    }
+  });
+}
